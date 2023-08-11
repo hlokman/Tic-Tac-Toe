2d050dced2920a2664b2d0592b95ee10168c09b5
hlokman <abdallahlokmanhenni@gmail.com>
2023-08-11 23:36:50 +0200

    Create modules and factory function

---
+++ TTT.js
@@ -0,0 +1,22 @@
+const gameBoard = (() => {
+
+    return {
+
+    };
+})();
+
+
+const displayController = (() => {
+
+    return {
+
+    };
+})();
+
+
+const player = (name, marker) => {
+
+    return {name, marker};
+};
+
+//gameboard as an array inside of a Gameboard object
