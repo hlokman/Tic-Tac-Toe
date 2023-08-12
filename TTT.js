e2a3bc67fe2c0bacc03eb74e40add08a4f74cb42
hlokman <abdallahlokmanhenni@gmail.com>
2023-08-12 02:19:20 +0200

    Edit the files

--- TTT.js
+++ TTT.js
@@ -1,7 +1,10 @@
+//const array = ['X'];
+//gameBoard.array[6] = 'O';
+
 const gameBoard = (() => {
-
+    const array = [];
     return {
-
+        array
     };
 })();
 
@@ -20,3 +23,4 @@ const player = (name, marker) => {
 };
 
 //gameboard as an array inside of a Gameboard object
+//if you only ever need ONE of something (gameBoard, displayController), use a module. If you need multiples of something (players!), create them with factories.
