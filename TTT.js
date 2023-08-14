67a774c628bf5da72891374b2ff58d9899a72eba
hlokman <abdallahlokmanhenni@gmail.com>
2023-08-14 22:15:27 +0200

    Reorganize the modules

--- TTT.js
+++ TTT.js
@@ -3,44 +3,6 @@
 const board = document.querySelector('#gameboard');
 //const array = [];
 
-//-------------------GAME FLOW--------------------------------
-const gameFlow = (() => {
-    board.addEventListener('click', (e) => {
-    console.log(e.target.dataset.indexNumber);
-    //console.log(gameBoard.array[e.target.dataset.indexNumber]);
-    if (gameBoard.array[e.target.dataset.indexNumber] == undefined) {
-        gameBoard.array[e.target.dataset.indexNumber] = 'X';
-        console.log(gameBoard.array)
-        e.target.textContent = 'X'
-    }
-    });
-
-    return {
-        
-    };
-})();
-
-//-------------------GAME BOARD--------------------------------
-const gameBoard = (() => {
-    const array = [];
-    //const array = ['X','O','O','X','X','O','X','O','X'];
-    return {
-        array
-    };
-})();
-
-//----------------DISPLAY CONTROLLER---------------------------
-const displayController = (() => {
-    const display = function(array) {
-        return test.textContent = array;
-    };
-    
-    return {
-        //array,
-        display
-    };
-})();
-
 //--------------------CREATE PLAYERS----------------------------
 const player = (name, marker) => {
     return {name, marker};
@@ -49,8 +11,56 @@ const playerOne = player('One', 'X');
 const playerTwo = player('Two', 'O');
 
 
+
+//-------------------GAME BOARD--------------------------------
+const gameBoard = (() => {
+    const array = [];
+    const getArray = () => array;
+    //const array = ['X','O','O','X','X','O','X','O','X'];
+    return {
+        getArray
+    };
+})();
+
+
+//-------------------GAME FLOW--------------------------------
+const gameFlow = (() => {
+
+
+    return {
+
+    };
+})();
+
+
+//----------------DISPLAY CONTROLLER---------------------------
+const displayController = (() => {
+    //const board = gameBoard;
+    //console.log(board.getArray())
+
+    board.addEventListener('click', (e) => {
+        console.log(e.target.dataset.indexNumber);
+        //console.log(gameBoard.array[e.target.dataset.indexNumber]);
+        //Change gameBoard.getArray() to board.getArray() 
+        // ==> module was used to create gameBoard, so ne need to simplify gameBoard to board?
+        if (gameBoard.getArray()[e.target.dataset.indexNumber] == undefined) {
+            gameBoard.getArray()[e.target.dataset.indexNumber] = 'X';
+            console.log(gameBoard.getArray())
+            e.target.textContent = 'X'
+        }
+        });
+    return {};
+    // We don't need to return anything from this module because everything is encapsulated inside this screen controller.
+})();
+
+
+
+
 //gameboard as an array inside of a Gameboard object
 //if you only ever need ONE of something (gameBoard, displayController), use a module. If you need multiples of something (players!), create them with factories.
+//module called ScreenController. This module will leverage an updateScreen pattern
+
+
 
 //const test = document.querySelector('.test');
 //displayController.display(array)
