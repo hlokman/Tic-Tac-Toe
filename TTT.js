0aa372f596fd76897bc428b76fbc4083f8a8d85b
hlokman <abdallahlokmanhenni@gmail.com>
2023-08-20 00:01:52 +0200

    Finalize the game

--- TTT.js
+++ TTT.js
@@ -3,14 +3,18 @@
 const board = document.querySelector('#gameboard');
 const subBoard = document.querySelectorAll('#cell');
 const resultsBox = document.querySelector('.results');
+const restart = document.querySelector('.restart');
+const names = document.querySelector('.names');
+const player1 = document.querySelector('.player1');
+const player2 = document.querySelector('.player2');
 //const array = [];
 
 //--------------------CREATE PLAYERS----------------------------
 const player = (name, marker) => {
     return {name, marker};
 };
-const playerOne = player('Player one', 'X');
-const playerTwo = player('Player two', 'O');
+const playerOne = player('Player 1', 'X');
+const playerTwo = player('Player 2', 'O');
 const players = [playerOne, playerTwo];
 
 //players[0].name = prompt("Player one's name", 'Player one');
@@ -31,6 +35,8 @@ const gameBoard = (() => {
 
 //-------------------GAME FLOW--------------------------------
 const gameFlow = (() => {
+
+
     //console.log(players[0]);
     let activePlayer = players[0];
 
@@ -60,12 +66,12 @@ const gameFlow = (() => {
             controller.abort();
             gameFlow.round(); //trick to get the correct player to render afterward
             resultsBox.textContent = '';
-            resultsBox.textContent = `${gameFlow.getActivePlayer().name} is the winner !!!`
+            resultsBox.innerHTML = `CONGRATULATIONS!<br>${gameFlow.getActivePlayer().name} wins`
         } else if (gameBoard.getArray().length === 9 && gameBoard.getArray().includes(undefined) === false) {
             console.log('TIE');
             controller.abort();
             resultsBox.textContent = '';
-            resultsBox.textContent = `It's a tie !!!!`
+            resultsBox.textContent = `It's    a    tie !`
         }
     };
     const getController = () => controller;
@@ -86,12 +92,48 @@ const displayController = (() => {
             for (let i = 0; i <= 8; i++) {
                 //console.log(subBoard[i].attributes[1].value)
                 if (subBoard[i].attributes[1].value == index) {
-                    subBoard[i].innerText = item;
+                    if (item == 'X') {
+                        subBoard[i].innerHTML = '<img src="./images/icons8-x.svg" class="mark">';
+                    } else if (item == 'O') {
+                        subBoard[i].innerHTML = '<img src="./images/circle-xxs-svgrepo-com.svg" class="mark" id="circle"></img>';
+                    }
+                    
                 }
             }
         })
     };
     screenUpdate(); 
+
+
+    //to render the names on screen-----------------
+    player1.textContent = `${players[0].name}`;
+    player2.textContent = `${players[1].name}`;
+    //to restart the game
+    restart.addEventListener('click', () => {
+        document.location.reload();
+    });
+    //to change the names
+    names.addEventListener('click', () => {
+        if (gameBoard.getArray().length === 0) {
+            players[0].name = prompt("Player 1's name", 'Player 1');
+            while (players[0].name.length > 30) {
+                alert('please choose a name under 30 characters')
+                players[0].name = prompt("Player 1's name", 'Player 1');
+            }
+            players[1].name = prompt("Player 2's name", 'Player 2');
+            while (players[1].name.length > 30) {
+                alert('please choose a name under 30 characters')
+                players[1].name = prompt("Player 1's name", 'Player 1');
+            }
+            player1.textContent = `${players[0].name}`;
+            player2.textContent = `${players[1].name}`;
+            resultsBox.textContent = '';
+            resultsBox.textContent = `${players[0].name}'s turn`
+        } else {
+            alert('You can not change the names mid-game')
+        }
+    })
+    //---------------------------------------------
 
     resultsBox.textContent = `${gameFlow.getActivePlayer().name}'s turn`
 
